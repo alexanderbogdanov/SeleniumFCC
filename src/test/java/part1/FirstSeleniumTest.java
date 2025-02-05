e03289d7389b8e0f48c0ceccb705ed55c619ff1e
alexandrbogdanov <ironweed@list.ru>
2025-02-05 22:52:06 +0100

    added logging: slf4j and logback

--- src/test/java/part1/FirstSeleniumTest.java
+++ src/test/java/part1/FirstSeleniumTest.java
@@ -1,4 +1,40 @@
 package part1;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class FirstSeleniumTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void seUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testLoggingIntoApplication() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("form")));
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("Admin");
+        var password =  driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+        driver.findElement(By.tagName("button")).click();
+    }
 }
