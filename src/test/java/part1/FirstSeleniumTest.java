da4cb2a6000545b74766615d07cf3d4bc4704a6d
alexandrbogdanov <ironweed@list.ru>
2025-05-24 17:25:27 +0200

    minor tweaks

--- src/test/java/part1/FirstSeleniumTest.java
+++ src/test/java/part1/FirstSeleniumTest.java
@@ -1,5 +1,6 @@
 package part1;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,36 +11,48 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
 public class FirstSeleniumTest {
     WebDriver driver;
+    Logger logger = LoggerFactory.getLogger(FirstSeleniumTest.class);
 
     @BeforeClass
     public void setUp() {
+        logger.info("Setting up browser");
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        logger.info("Navigated to login page");
     }
 
     @AfterClass
     public void tearDown() {
+        logger.info("Closing browser");
         driver.quit();
     }
 
     @Test
     public void testLoggingIntoApplication() {
+        logger.info("Starting test: testLoggingIntoApplication");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("form")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("form.oxd-form")));
         WebElement username = driver.findElement(By.name("username"));
         username.sendKeys("Admin");
+        logger.info("Entered username");
         var password =  driver.findElement(By.name("password"));
         password.sendKeys("admin123");
-        driver.findElement(By.tagName("button")).click();
+        logger.info("Entered password");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        logger.info("Clicked login button");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("h6")));
         String actualResult = driver.findElement(By.tagName("h6")).getText();
         String expectedResult = "Dashboard";
         Assert.assertEquals(actualResult, expectedResult);
+        logger.info("Assertion passed, test successful");
     }
 }
